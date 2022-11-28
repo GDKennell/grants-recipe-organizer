08a84213925b7ef3d1694c0aa724ed10fd78bd84
Grant Kennell <grant.h.kennell@gmail.com>
2022-11-27 19:41:05 -0800

    Got parsing of volume measurements and cups conversion mostly working

--- src/RecipeConversion/convertRecipe.js
+++ src/RecipeConversion/convertRecipe.js
@@ -1,5 +1,9 @@
 import { wordsToNumbers } from "./utilities/numberConversion";
 
+/** ***************************** */
+/*    Public Function        */
+/** ***************************** */
+
 /**
  * @param {string} recipeStringIn
  */
@@ -8,6 +12,25 @@ export function convertRecipe(recipeStringIn) {
   const newLines = lines.map(convertLine);
   return newLines.join("\n");
 }
+
+/** ***************************** */
+/*    General Helpers      */
+/** ***************************** */
+function stringContains(haystack, needle) {
+  return haystack.indexOf(needle) != -1;
+}
+
+function stringContainsWord(haystack, needle) {
+  const index = haystack.indexOf(needle);
+  const endIndex = index + needle.length;
+  return (
+    index != -1 && (endIndex >= haystack.length || haystack[endIndex] == " ")
+  );
+}
+
+/** ***************************** */
+/*    Data & Data Structure      */
+/** ***************************** */
 
 class VolumeMeasure {
   /**
@@ -27,19 +50,78 @@ const tablespoonMeasure = new VolumeMeasure(
   0.062499920209125003
 );
 
+/** ***************************** */
+/*    Volume type lists      */
+/** ***************************** */
+
 const allVolumeMeasurements = [cupMeasure, teaspoonMeasure, tablespoonMeasure];
 
+var allVolumeNameStrings = allVolumeMeasurements.flatMap((m) => m.names);
+allVolumeNameStrings = allVolumeNameStrings.flatMap((str) => [str, str + "s"]);
+
+var nameToVolume = {};
+for (const volume of allVolumeMeasurements) {
+  for (const name of volume.names) {
+    nameToVolume[name] = volume;
+    nameToVolume[name + "s"] = volume;
+  }
+}
+console.log(nameToVolume);
+
+/** ***************************** */
+/*    Parsing Helpers       */
+/** ***************************** */
+
 function containsVolumeMeasurement(line) {
-  var allStrings = allVolumeMeasurements.flatMap((m) => m.names);
-  allStrings = allStrings.flatMap((str) => [str, str + "s"]);
   const lowerLine = line.toLocaleLowerCase();
-  for (const str of allStrings) {
-    if (lowerLine.indexOf(str) != -1) {
+  for (const str of allVolumeNameStrings) {
+    if (stringContainsWord(lowerLine, str)) {
       return true;
     }
   }
   return false;
 }
+
+function findVolumeString(line) {
+  const lowerLine = line.toLocaleLowerCase();
+  var volumeNamePos = -1;
+  var volumeTypeString = "";
+  var volumeType = null;
+  for (const str of allVolumeNameStrings) {
+    if (stringContainsWord(lowerLine, str)) {
+      volumeNamePos = lowerLine.indexOf(str);
+      volumeType = nameToVolume[str];
+      volumeTypeString = str;
+      break;
+    }
+  }
+  const volumeStringEndIndex = volumeNamePos + volumeTypeString.length;
+  const allowedNumberChars = "0123456789./ ";
+  // Step back from start of name string to find last non-number char\
+  var testIndex = volumeNamePos - 1;
+  while (
+    testIndex >= 0 &&
+    stringContains(allowedNumberChars, lowerLine[testIndex])
+  ) {
+    testIndex--;
+  }
+  // Step forward to last actually valid char
+  if (!stringContains(allowedNumberChars, lowerLine[testIndex])) {
+    testIndex++;
+  }
+  // Step forward past any whitespace
+  while (lowerLine[testIndex] == " ") {
+    testIndex++;
+  }
+  // testIndex is now start of number sequence
+  const volumeStringStartIndex = testIndex;
+  const numberString = line.substring(testIndex, volumeNamePos);
+  const volumeAmount = parseFloat(numberString);
+  const volumeInCups = volumeAmount * volumeType.ratioToCup;
+  return [volumeStringStartIndex, volumeInCups, volumeStringEndIndex];
+}
+
+function findIngredientName(lineIn, volumeStringEndIndex) {}
 
 /**
  * @param {string} lineIn
@@ -50,5 +132,13 @@ function convertLine(lineIn) {
   }
   console.log("converting ", lineIn);
   var newLine = wordsToNumbers(lineIn);
-  return newLine;
+  const [volumeStringStartIndex, volumeInCups, volumeStringEndIndex] =
+    findVolumeString(newLine);
+  const cupsValueString = volumeInCups.toFixed(4) + " cups";
+  const finalLine =
+    lineIn.slice(0, volumeStringStartIndex) +
+    cupsValueString +
+    lineIn.slice(volumeStringEndIndex);
+  // const ingredientName = findIngredientName(lineIn, volumeStringEndIndex)
+  return finalLine;
 }
