e0f664b431209ea2baa86b1a84f24003ca0c7a62
Grant Kennell <grant.h.kennell@gmail.com>
2022-11-27 22:29:33 -0800

    Re-enabled text field

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   const initialText =
     "\nPie 101 page \n\npie crust 101 – smitten kitchen\n\n- 2.5 cups flour\n- 1 tsp salt\n- Two Tbsp sugar \n- 2 cups butter \n- 1/4 cup water\n- 1/4 cup vodka \n\n";
   const initialLines = initialText.split("\n").length;
-  const [outputText, setOutputText] = useState(convertRecipe(initialText));
-  const [numRows, setNumRows] = useState(initialLines);
+  const [outputText, setOutputText] = useState("");
+  const [numRows, setNumRows] = useState(1);
   const textAreaChange = (event) => {
     const textInput = event.target.value;
     const newValue = convertRecipe(textInput);
@@ -21,7 +21,6 @@ function App() {
           className="recipe-input-field"
           rows={numRows}
           onChange={textAreaChange}
-          value={initialText}
         ></textarea>
         <div className="output-field">{outputText}</div>
       </div>
