930edc56d152f0c79de57d989e2dfc40da1aa84b
Grant Kennell <grant.h.kennell@gmail.com>
2022-11-28 10:50:51 -0800

    Added test cases from pie 101; modifiers, punctuation

--- src/RecipeConversion/convertRecipe.test.js
+++ src/RecipeConversion/convertRecipe.test.js
@@ -21,3 +21,32 @@ test("converts values to cups", () => {
     expect(result).toEqual(testCase[1]);
   }
 });
+
+test("handles modifiers to ingredients and extraneous notes", () => {
+  const testCases = [
+    ["2 1/2 cups (12 1/2 ounces) unbleached all-purpose flour", ""],
+    ["1 teaspoon table salt", ""],
+    ["2 tablespoons sugar", ""],
+    [
+      "12 tablespoons (1 1/2 sticks) cold unsalted butter, cut into 1/4-inch slices",
+      "",
+    ],
+    ["1/2 cup cold vegetable shortening, cut into small bits", ""],
+    ["1/4 cup cold vodka", ""],
+    ["1/4 cup  cold water", ""],
+  ];
+  /**
+ * 312.5g (2.50 cups)  flour
+6.0g (1 teaspoon)  salt
+24.7g (2 tablespoons)  sugar
+170.2g (12 tablespoons)  butter , cut into 0.25-inch slices
+102.5g (0.50 cup)  shortening , cut into small bits
+56.0g (0.25 cup)  vodka
+59.1g (0.25 cup)  wate
+ */
+  for (const testCase of testCases) {
+    //   console.log("testing " + testCase[0]);
+    const result = convertLine(testCase[0]);
+    expect(result).toEqual(testCase[1]);
+  }
+});
