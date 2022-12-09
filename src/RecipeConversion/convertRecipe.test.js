b1a68f06b1ab52a1b647d0e23d97198af347d358
Grant Kennell <grant.h.kennell@gmail.com>
2022-12-09 14:17:26 -0600

    Uncommented some tests that are passing

--- src/RecipeConversion/convertRecipe.test.js
+++ src/RecipeConversion/convertRecipe.test.js
@@ -14,160 +14,136 @@ test('object test', () => {
 
 const ingredientManager = globalIngredientManager;
 
-// test('converts values to cups', () => {
-//   const testCases = [
-//     ['- 1 tsp salt', '- 6g (1 tsp) salt'],
-//     ['- Two Tbsp sugar ', '- 24.7g (2 Tbsp) sugar '],
-//     ['- 2 cups butter ', '- 454g (2 cups) butter '],
-//     ['- 1/4 cup water', '- 59.1g (0.25 cup) water'],
-//     ['- 1/4 cup vodka ', '- 56g (0.25 cup) vodka '],
-//     ['- 2 1/4 cup rum', '- 504g (2.25 cup) rum'],
-//     ['- 2 1/4 cups shortening', '- 461.3g (2.25 cups) shortening'],
-//     ['3/16 tablespoons Beer', '2.8g (0.19 tablespoons) Beer'],
-//     ['3 1/4 cup tequila', '728g (3.25 cup) tequila'],
-//   ];
 
+test('converts values to cups', () => {
+  const testCases = [
+    ['- 1 tsp salt', '- 6g (1 tsp) salt'],
+    ['- Two Tbsp sugar ', '- 24.7g (2 Tbsp) sugar '],
+    ['- 2 cups butter ', '- 454g (2 cups) butter '],
+    ['- 1/4 cup water', '- 59.1g (0.25 cup) water'],
+    ['- 1/4 cup vodka ', '- 56g (0.25 cup) vodka '],
+    ['- 2 1/4 cup rum', '- 504g (2.25 cup) rum'],
+    ['- 2 1/4 cups shortening', '- 461.3g (2.25 cups) shortening'],
+    ['3/16 tablespoons Beer', '2.8g (0.19 tablespoons) Beer'],
+    ['3 1/4 cup tequila', '728g (3.25 cup) tequila'],
+  ];
+
+  for (const testCase of testCases) {
+    const [result] = parseIngredientListLine(testCase[0], ingredientManager);
+    expect(result).toEqual(testCase[1]);
+  }
+});
+
+
+test('2.5 cups flour', () => {
+  const testCases = [['- 2.5 cups flour', '- 312.5g (2.5 cups) flour']];
+  for (const testCase of testCases) {
+    const [result] = parseIngredientListLine(testCase[0], ingredientManager);
+    expect(result).toEqual(testCase[1]);
+  }
+});
+
+test('oz weight ', () => {
+  const testCases = [
+    ['▢ 10.6 oz cream cheese', '▢ 300.5g (10.6 oz) cream cheese'],
+  ];
+  for (const testCase of testCases) {
+    const [result] = parseIngredientListLine(testCase[0], ingredientManager);
+    expect(result).toEqual(testCase[1]);
+  }
+});
+
+// test('1 ½ tsp baking powder', () => {
+//   const testCases = [['1 ½ tsp baking powder', '']];
 //   for (const testCase of testCases) {
 //     const [result] = parseIngredientListLine(testCase[0], ingredientManager);
 //     expect(result).toEqual(testCase[1]);
 //   }
 // });
 
-// test('2.5 cups flour', () => {
-//   const testCases = [['- 2.5 cups flour', '- 312.5g (2.5 cups) flour']];
-//   for (const testCase of testCases) {
-//     const [result] = parseIngredientListLine(testCase[0], ingredientManager);
-//     expect(result).toEqual(testCase[1]);
-//   }
-// });
+test('handles modifiers to ingredients and extraneous notes', () => {
+  const testCases = [
+    [
+      '2 1/2 cups (12 1/2 ounces) unbleached all-purpose flour',
+      '312.5g (2.50 cups) (12 1/2 ounces) unbleached all-purpose flour',
+    ],
+    ['1 teaspoon table salt', '6g (1 teaspoon) table salt'],
+    ['2 tablespoons sugar', '24.7g (2 tablespoons) sugar'],
+    [
+      '12 tablespoons (1 1/2 sticks) cold unsalted butter, cut into 1/4-inch slices',
+      '170.2g (12 tablespoons) (1.50 sticks) cold unsalted butter, cut into 1/4-inch slices',
+    ],
 
-// test('oz weight ', () => {
-//   const testCases = [
-//     ['▢ 10.6 oz cream cheese', '▢ 300.5g (10.6 oz) cream cheese'],
-//   ];
-//   for (const testCase of testCases) {
-//     const [result] = parseIngredientListLine(testCase[0], ingredientManager);
-//     expect(result).toEqual(testCase[1]);
-//   }
-// });
+    ['1/4 cup cold vodka', '56g (0.25 cup) cold vodka'],
+    ['1/4 cup  cold water', '59.1g (0.25 cup)  cold water'],
+  ];
 
-// // test("1 ½ tsp baking powder", () => {
-// //   const testCases = [["1 ½ tsp baking powder", ""]];
-// //   for (const testCase of testCases) {
-// //     const [result,] = parseIngredientListLine(testCase[0], ingredientManager);
-// //     expect(result).toEqual(testCase[1]);
-// //   }
-// // });
+  for (const testCase of testCases) {
+    //   console.log("testing " + testCase[0]);
+    const [result] = parseIngredientListLine(testCase[0], ingredientManager);
+    expect(result).toEqual(testCase[1]);
+  }
+});
 
-// test('handles modifiers to ingredients and extraneous notes', () => {
-//   const testCases = [
-//     [
-//       '2 1/2 cups (12 1/2 ounces) unbleached all-purpose flour',
-//       '312.5g (2.50 cups) (12 1/2 ounces) unbleached all-purpose flour',
-//     ],
-//     ['1 teaspoon table salt', '6g (1 teaspoon) table salt'],
-//     ['2 tablespoons sugar', '24.7g (2 tablespoons) sugar'],
-//     [
-//       '12 tablespoons (1 1/2 sticks) cold unsalted butter, cut into 1/4-inch slices',
-//       '170.2g (12 tablespoons) (1.50 sticks) cold unsalted butter, cut into 1/4-inch slices',
-//     ],
+test('Half cup cold shortening', () => {
+  const testCases = [
+    [
+      '1/2 cup cold vegetable shortening, cut into small bits',
+      '102.5g (0.50 cup) cold vegetable shortening, cut into small bits',
+    ],
+  ];
 
-//     ['1/4 cup cold vodka', '56g (0.25 cup) cold vodka'],
-//     ['1/4 cup  cold water', '59.1g (0.25 cup)  cold water'],
-//   ];
+  for (const testCase of testCases) {
+    //   console.log("testing " + testCase[0]);
+    const [result] = parseIngredientListLine(testCase[0], ingredientManager);
+    expect(result).toEqual(testCase[1]);
+  }
+});
 
-//   for (const testCase of testCases) {
-//     //   console.log("testing " + testCase[0]);
-//     const [result] = parseIngredientListLine(testCase[0], ingredientManager);
-//     expect(result).toEqual(testCase[1]);
-//   }
-// });
+test('cold water = water', () => {
+  const [result] = parseIngredientListLine('1/4 cup  cold water', ingredientManager);
+  expect(result).toEqual('59.1g (0.25 cup)  cold water');
+});
+test('handles unicode fractions', () => {
+  const testCases = [
+    [
+      '1½ cups/180 grams all-purpose flour',
+      '187.5g (1.50 cups)/180 grams all-purpose flour',
+    ],
+    ['½ teaspoon baking soda', '2.3g (0.50 teaspoon) baking soda'],
+    ['½ teaspoon kosher salt', '3g (0.50 teaspoon) kosher salt'],
+    [
+      '10 tablespoons/140 grams unsalted butter (1¼ sticks), at room temperature',
+      '141.9g (10 tablespoons)/140 grams unsalted butter (1 1/4 sticks), at room temperature',
+    ],
+    [
+      '¾ cup/150 grams granulated sugar, ',
+      '148.5g (0.75 cup)/150 grams granulated sugar, ',
+    ],
+    ['1 large egg', '1 large egg'],
+    ['½ teaspoon vanilla extract', '2.2g (0.50 teaspoon) vanilla extract'],
+    ['1 tablespoon ground cinnamon', '7.8g (1 tablespoon) ground cinnamon'],
+  ];
 
-// test('Half cup cold shortening', () => {
-//   const testCases = [
-//     [
-//       '1/2 cup cold vegetable shortening, cut into small bits',
-//       '102.5g (0.50 cup) cold vegetable shortening, cut into small bits',
-//     ],
-//   ];
+  for (const testCase of testCases) {
+    //   console.log("testing " + testCase[0]);
+    const [result] = parseIngredientListLine(testCase[0], ingredientManager);
+    expect(result).toEqual(testCase[1]);
+  }
+});
 
-//   for (const testCase of testCases) {
-//     //   console.log("testing " + testCase[0]);
-//     const [result] = parseIngredientListLine(testCase[0], ingredientManager);
-//     expect(result).toEqual(testCase[1]);
-//   }
-// });
+test('cream vs cream of tartar', () => {
+  const testCases = [
+    ['1 teaspoon cream of tartar', '3g (1 teaspoon) cream of tartar'],
+  ];
 
-// test('cold water = water', () => {
-//   const [result] = parseIngredientListLine('1/4 cup  cold water', ingredientManager);
-//   expect(result).toEqual('59.1g (0.25 cup)  cold water');
-// });
-// test('handles unicode fractions', () => {
-//   const testCases = [
-//     [
-//       '1½ cups/180 grams all-purpose flour',
-//       '187.5g (1.50 cups)/180 grams all-purpose flour',
-//     ],
-//     ['½ teaspoon baking soda', '2.3g (0.50 teaspoon) baking soda'],
-//     ['½ teaspoon kosher salt', '3g (0.50 teaspoon) kosher salt'],
-//     [
-//       '10 tablespoons/140 grams unsalted butter (1¼ sticks), at room temperature',
-//       '141.9g (10 tablespoons)/140 grams unsalted butter (1 1/4 sticks), at room temperature',
-//     ],
-//     [
-//       '¾ cup/150 grams granulated sugar, ',
-//       '148.5g (0.75 cup)/150 grams granulated sugar, ',
-//     ],
-//     ['1 large egg', '1 large egg'],
-//     ['½ teaspoon vanilla extract', '2.2g (0.50 teaspoon) vanilla extract'],
-//     ['1 tablespoon ground cinnamon', '7.8g (1 tablespoon) ground cinnamon'],
-//   ];
+  for (const testCase of testCases) {
+    //   console.log("testing " + testCase[0]);
+    const [result] = parseIngredientListLine(testCase[0], ingredientManager);
+    expect(result).toEqual(testCase[1]);
+  }
+});
 
-//   for (const testCase of testCases) {
-//     //   console.log("testing " + testCase[0]);
-//     const [result] = parseIngredientListLine(testCase[0], ingredientManager);
-//     expect(result).toEqual(testCase[1]);
-//   }
-// });
-
-// test('cream vs cream of tartar', () => {
-//   const testCases = [
-//     ['1 teaspoon cream of tartar', '3g (1 teaspoon) cream of tartar'],
-//   ];
-
-//   for (const testCase of testCases) {
-//     //   console.log("testing " + testCase[0]);
-//     const [result] = parseIngredientListLine(testCase[0], ingredientManager);
-//     expect(result).toEqual(testCase[1]);
-//   }
-// });
-// test('puts ingredients on new lines', () => {
-//   const prepSteps =
-//     'In the bowl of an electric mixer, beat together the butter and ¾ cup sugar until fluffy, about 2 minutes, scraping down the sides as necessary. Beat in the egg until creamy, and then add the vanilla, again scraping down the sides. Add the flour mixture to the butter mixture and beat on low until just combined.';
-
-//   const expected =
-//     '> In the bowl of an electric mixer \n' +
-//     '>  - -  beat together the \n' +
-//     '>  - butter\n' +
-//     '>  - 0.75 cup sugar\n' +
-//     '>  until fluffy \n' +
-//     '>  - -  about 2 minutes \n' +
-//     '>  - -  scraping down the sides as necessary \n' +
-//     '>   Beat in the \n' +
-//     '>  - egg\n' +
-//     '>  until creamy \n' + // Todo: want this indented
-//     '>  - -  and add the \n' +
-//     '>  - vanilla\n' +
-//     ' \n' + // Todo: fix thi
-//     '>  - -  again scraping down the sides \n' +
-//     '>   Add the \n' +
-//     '>  - flour\n' +
-//     '>  mixture to the \n' +
-//     '>  - butter\n' +
-//     '>  mixture and beat on low until just combined';
-//   const result = parseRecipe(prepSteps, ingredientManager);
-//   expect(result).toEqual(expected);
-// });
 
 // test('looks for longer ingredient if present', () => {
 //   const prepSteps =
