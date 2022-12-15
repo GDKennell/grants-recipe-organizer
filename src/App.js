8597542935402de403c52b64b252628deb007116
Grant Kennell <grantkennell@Grants-MacBook-Air.local>
2022-12-15 12:16:45 -0500

    Added placeholder login page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import {useEffect, useState} from 'react';
 import './App.css';
 import {globalFirebaseManager} from './FirebaseManager';
 import IngredientPage from './IngredientPage';
+import LoginPage from './LoginPage';
 import RecipeConversion from './RecipeConversion';
 
 const PageType = {
   RECIPE_CONVERSION: 0,
   INGREDIENTS_PAGE: 1,
+  LOGIN_PAGE: 2,
 };
 
+const pageTypes = [PageType.RECIPE_CONVERSION, PageType.INGREDIENTS_PAGE, PageType.LOGIN_PAGE];
 
 function getCurrentComponent(pageType) {
   switch (pageType) {
@@ -17,32 +20,39 @@ function getCurrentComponent(pageType) {
       return <RecipeConversion />;
     case PageType.INGREDIENTS_PAGE:
       return <IngredientPage />;
+    case PageType.LOGIN_PAGE:
+      return <LoginPage/>;
   }
   return <div />;
 }
+
+const nameforPageType = (pageType) => {
+  switch (pageType) {
+    case PageType.RECIPE_CONVERSION:
+      return 'Recipe Conversion';
+    case PageType.INGREDIENTS_PAGE:
+      return 'Ingredients';
+    case PageType.LOGIN_PAGE:
+      return 'Login';
+  }
+};
+
 function App() {
   const [showingPage, setShowingPage] = useState(PageType.RECIPE_CONVERSION);
   useEffect(() => {
     globalFirebaseManager.initialize();
   }, []);
 
-  const toggleButtonPressed = () => {
-    console.log('Button');
-    if (showingPage == PageType.RECIPE_CONVERSION) {
-      setShowingPage(PageType.INGREDIENTS_PAGE);
-    } else {
-      setShowingPage(PageType.RECIPE_CONVERSION);
-    }
-  };
-  const getButtonText = () => {
-    if (showingPage == PageType.RECIPE_CONVERSION) {
-      return 'Ingredients Page';
-    }
-    return 'Recipe Conversion Page';
-  };
   return (
     <div className="App">
-      <button onClick={toggleButtonPressed} > {getButtonText()}</button>
+      {pageTypes.map((type) => {
+        return <button key={nameforPageType(type)}
+          onClick={ () => {
+            setShowingPage(type);
+          }} >
+          {nameforPageType(type)}
+        </button>;
+      }) }
       {getCurrentComponent(showingPage)}
     </div>
   );
