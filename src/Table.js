81571602d58f5880ed0525340250ce36622483f4
Grant Kennell <grantkennell@Grants-MacBook-Air.local>
2022-12-23 08:51:48 -0500

    Moving custom hook to table and params to rows. Now having ~600 ing managers, not thousands

--- src/Table.js
+++ src/Table.js
@@ -1,14 +1,27 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import useFirebase from './hooks/useFirebase';
+import useIngredientsStore from './hooks/useIngredientsStore';
 import TableRow from './TableRow';
 
 export default function Table({data}) {
+  const {ingredientManager} = useIngredientsStore();
+  const {firebaseUser} = useFirebase();
+
   const headerData = {names: ['Ingredient Name(s)'], gramsPerCup: 'Grams per Cup'};
   return (
     <table style= {{border: '1px solid'}}>
       <tbody>
-        <TableRow rowData={headerData} key = {'heyimakeygoshdarnit'} />
-        {data.map((rowData) => <TableRow rowData={rowData} key={rowData.key}/>)}
+        <TableRow rowData={headerData}
+          key = {'heyimakeygoshdarnit'}
+          firebaseUser={firebaseUser}
+          ingredientManager={ingredientManager}/>
+        {data.map((rowData) =>
+          <TableRow rowData={rowData}
+            key={rowData.key}
+            firebaseUser={firebaseUser}
+            ingredientManager={ingredientManager}/>,
+        )}
       </tbody>
     </table>
   );
--- src/TableRow.js
+++ src/TableRow.js
@@ -1,12 +1,8 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import useFirebase from './hooks/useFirebase';
-import useIngredientsStore from './hooks/useIngredientsStore';
 import {isIngredientOwned} from './RecipeConversion/DataStructures/ingredient';
 
-export default function TableRow({rowData, key}) {
-  const {ingredientManager} = useIngredientsStore();
-  const {firebaseUser} = useFirebase();
+export default function TableRow({rowData, firebaseUser, ingredientManager, key}) {
   const isEditable = (isIngredientOwned(rowData, firebaseUser));
   const editableIngredientsExist = (ingredientManager.getUserScopedIngredients(firebaseUser).length > 0);
 
