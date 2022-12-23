37d7e60bcea630c36098eeeae82ea1e11347217f
Grant Kennell <grantkennell@Grants-MacBook-Air.local>
2022-12-23 08:44:22 -0500

    Added useMemo for useIngredientStore. Fixed millions of ing managers on recipe page, but still bad on table pages

--- src/hooks/useIngredientsStore.js
+++ src/hooks/useIngredientsStore.js
@@ -1,12 +1,18 @@
+import {useMemo} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 
 import {IngredientManager} from '../RecipeConversion/DataStructures/ingredient';
 
+
 const useIngredientsStore = () => {
   const dispatch = useDispatch();
   const ingredientList = useSelector((state) => state.ingredientStore.ingredientList);
+  const ingredientManager = useMemo(() => {
+    return new IngredientManager(ingredientList);
+  }, [ingredientList]);
 
-  return {ingredientManager: new IngredientManager(ingredientList),
+  console.log(`useIngredientsStore hook making new IngredientManager(${ingredientList.length})`);
+  return {ingredientManager: ingredientManager,
     dispatch: dispatch};
 };
 
